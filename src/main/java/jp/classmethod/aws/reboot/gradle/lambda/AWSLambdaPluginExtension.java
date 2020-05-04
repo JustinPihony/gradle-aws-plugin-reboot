32b78ea48b492cc0a6041eb06aaf012525280b86
Justin Pihony <justin.pihony@gmail.com>
2020-05-04 00:05:45 -0400

    spotless

--- src/main/java/jp/classmethod/aws/reboot/gradle/lambda/AWSLambdaPluginExtension.java
+++ src/main/java/jp/classmethod/aws/reboot/gradle/lambda/AWSLambdaPluginExtension.java
@@ -40,11 +40,11 @@ public class AWSLambdaPluginExtension extends BaseRegionAwarePluginExtension<AWS
 	@Getter
 	@Setter
 	private Integer clientExecutionTimeout = -1;
-
+	
 	@Getter
 	@Setter
 	private Integer connectionTimeout = -1;
-
+	
 	@Getter
 	@Setter
 	private Integer socketTimeout = -1;
@@ -68,11 +68,11 @@ public class AWSLambdaPluginExtension extends BaseRegionAwarePluginExtension<AWS
 		if (clientExecutionTimeout > 0) {
 			clientConfiguration.setClientExecutionTimeout(clientExecutionTimeout);
 		}
-
+		
 		if (socketTimeout > 0) {
 			clientConfiguration.setSocketTimeout(socketTimeout);
 		}
-
+		
 		if (connectionTimeout > 0) {
 			clientConfiguration.setConnectionTimeout(connectionTimeout);
 		}
